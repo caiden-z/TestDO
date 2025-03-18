afec472421d635334d3fbfb7bc523c39fd0bb4d6
caiden-z <150832548+caiden-z@users.noreply.github.com>
2025-03-17 22:08:17 -0400

    reset

--- TestProject/django_app/django_app/settings.py
+++ TestProject/django_app/django_app/settings.py
@@ -142,9 +142,6 @@ STATICFILES_DIRS = [
     BASE_DIR / 'common_static',
 ]
 
-
-DJANGO_SETTINGS_MODULE="django_app.settings"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
